195331e6e7cf5593247633c2e2207a724c5e74e3
Sergei Nikitin <nik.pasaran@gmail.com>
2015-11-23 20:10:39 +0300

    moar tests

--- tests/block.http.js
+++ tests/block.http.js
@@ -1,3 +1,7 @@
+var url_ = require( 'url' );
+
+var no = require( 'nommon' );
+
 var expect = require( 'expect.js' );
 
 var de = require( '../lib/blocks/de.block.http.js' );
@@ -10,8 +14,6 @@ var fake = new Fake();
 
 var base_url = 'http://127.0.0.1:8080';
 
-var hello_string = 'Hello, World';
-
 var port = require( './_port.js' )();
 
 //  ---------------------------------------------------------------------------------------------------------------  //
@@ -21,12 +23,14 @@ fake.listen( port, '0.0.0.0', function() {
     describe( 'block.http', function() {
         var n = 1;
 
-        it( 'url as a string', function( done ) {
+        it( 'options as a string', function( done ) {
             var path = `/block/http/${ n++ }`;
+
+            var content = 'Hello, World';
 
             fake.add( path, {
                 status_code: 200,
-                content: hello_string,
+                content: content,
             } );
 
             var block = new de.Block.Http( `${ base_url }${ path }` );
@@ -34,11 +38,210 @@ fake.listen( port, '0.0.0.0', function() {
             block.run()
                 .then( function( result ) {
                     expect( result ).to.be.a( de.Result.Value );
-                    expect( result.as_object() ).to.be( hello_string );
+                    expect( result.as_object() ).to.be( content );
 
                     done();
                 } );
         } );
+
+        it( 'options as a function', function( done ) {
+            var path = `/block/http/${ n++ }`;
+
+            var content = 'Hello, World';
+
+            fake.add( `${ path }/42`, {
+                status_code: 200,
+                content: content,
+            } );
+
+            var block = new de.Block.Http( function( params ) {
+                return {
+                    url: `${ base_url }${ path }/${ params.id }`
+                };
+            } );
+
+            block.run( { id: 42 } )
+                .then( function( result ) {
+                    expect( result ).to.be.a( de.Result.Value );
+                    expect( result.as_object() ).to.be( content );
+
+                    done();
+                } );
+        } );
+
+        it( 'options.url', function( done ) {
+            var path = `/block/http/${ n++ }`;
+
+            var content = 'Hello, World';
+
+            fake.add( path, {
+                status_code: 200,
+                content: content,
+            } );
+
+            var block = new de.Block.Http( {
+                url: `${ base_url }${ path }`
+            } );
+
+            block.run()
+                .then( function( result ) {
+                    expect( result ).to.be.a( de.Result.Value );
+                    expect( result.as_object() ).to.be( content );
+
+                    done();
+                } );
+        } );
+
+        it( 'options.only_meta', function( done ) {
+            var path = `/block/http/${ n++ }`;
+
+            var content = 'Hello, World';
+
+            fake.add( path, {
+                status_code: 200,
+                content: content,
+            } );
+
+            var block = new de.Block.Http( {
+                url: `${ base_url }${ path }`,
+                only_meta: true
+            } );
+
+            block.run()
+                .then( function( result ) {
+                    expect( result ).to.be.a( de.Result.Value );
+                    expect( result.as_object().status_code ).to.be( 200 );
+                    expect( result.as_object().headers[ 'content-type' ] ).to.be( 'text/plain' );
+
+                    done();
+                } );
+        } );
+
+        it( 'options.is_json', function( done ) {
+            var path = `/block/http/${ n++ }`;
+
+            var content = {
+                foo: 42
+            };
+
+            fake.add( path, {
+                status_code: 200,
+                content: JSON.stringify( content ),
+            } );
+
+            var block = new de.Block.Http( {
+                url: `${ base_url }${ path }`,
+                is_json: true
+            } );
+
+            block.run()
+                .then( function( result ) {
+                    expect( result ).to.be.a( de.Result.Value );
+                    expect( result.as_object() ).to.be.eql( content );
+
+                    done();
+                } );
+        } );
+
+        it( 'options.is_json and invalid json', function( done ) {
+            var path = `/block/http/${ n++ }`;
+
+            fake.add( path, {
+                status_code: 200,
+                content: '{"foo":'
+            } );
+
+            var block = new de.Block.Http( {
+                url: `${ base_url }${ path }`,
+                is_json: true
+            } );
+
+            block.run()
+                .then( function( result ) {
+                    expect( result ).to.be.a( de.Result.Error );
+                    //  FIXME: Брать коды ошибок из одного места.
+                    expect( result.as_object() ).to.be.eql( { id: 'JSON_PARSE_ERROR' } );
+
+                    done();
+                } );
+        } );
+
+        it( 'json content-type, without options.is_json', function( done ) {
+            var path = `/block/http/${ n++ }`;
+
+            var content = {
+                foo: 42
+            };
+
+            fake.add( path, {
+                status_code: 200,
+                content: content
+            } );
+
+            var block = new de.Block.Http( {
+                url: `${ base_url }${ path }`
+            } );
+
+            block.run()
+                .then( function( result ) {
+                    expect( result ).to.be.a( de.Result.Value );
+                    expect( result.as_object() ).to.be.eql( content );
+
+                    done();
+                } );
+        } );
+
+        it( 'options.url with { expr }', function( done ) {
+            var path = `/block/http/${ n++ }`;
+
+            var content = 'Hello, World';
+
+            fake.add( `${ path }/42`, {
+                status_code: 200,
+                content: content
+            } );
+
+            var block = new de.Block.Http( {
+                url: `${ base_url }${ path }/{ .id }`
+            } );
+
+            block.run( { id: 42 } )
+                .then( function( result ) {
+                    expect( result ).to.be.a( de.Result.Value );
+                    expect( result.as_object() ).to.be.eql( content );
+
+                    done();
+                } );
+        } );
+
+        it( 'options.query and GET request', function( done ) {
+            var path = `/block/http/${ n++ }`;
+
+            fake.add( path, function( req, res, data ) {
+                var query = url_.parse( req.url, true ).query;
+
+                expect( query ).to.be.eql( {
+                    foo: 42,
+                    quu: 24
+                } );
+
+                done();
+            } );
+
+            var block = new de.Block.Http( {
+                url: `${ base_url }${ path }`,
+                query: {
+                    foo: no.jpath.expr( '.bar' ),
+                    quu: 24
+                }
+            } );
+
+            block.run( {
+                foo: 24,
+                bar: 42
+            } );
+        } );
+
     } );
 
     run();
