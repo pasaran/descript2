21e4a6075a9458ed357a1015c47ed72810da33c7
Sergei Nikitin <nik.pasaran@gmail.com>
2016-03-04 17:16:54 +0300

    memcached

--- tests/options.cache.js
+++ tests/options.cache.js
@@ -3,14 +3,17 @@ var no = require( 'nommon' );
 var expect = require( 'expect.js' );
 
 var de = require( '../lib/index.js' );
-require( '../lib/de.cacher.js' );
+require( '../lib/de.cache.js' );
 
 var helpers = require( './_helpers.js' );
 
 //  ---------------------------------------------------------------------------------------------------------------  //
 
-var async_cache = new de.Cacher.BaseAsync();
-var sync_cache = new de.Cacher.BaseSync();
+var memcache = new de.Cache( '127.0.0.1:11211' );
+var test_async_cache = new de.Cache.TestAsync();
+var test_sync_cache = new de.Cache.TestSync();
+
+var cache = test_async_cache;
 
 //  ---------------------------------------------------------------------------------------------------------------  //
 
@@ -33,42 +36,42 @@ describe( 'options.cache', function() {
             }, 50 ),
             {
                 key: key,
-                maxage: 200
+                maxage: 1
             }
         );
 
         var context = new de.Context.Base( {
-            cache: async_cache
+            cache: cache
         } );
         context.run( block )
             .then( function( result ) {
-                expect( result ).to.be( _result );
+                expect( result ).to.be.eql( _result );
                 expect( _calls ).to.be( 1 );
             } );
 
         setTimeout( function() {
             var context = new de.Context.Base( {
-                cache: async_cache
+                cache: cache
             } );
             context.run( block )
                 .then( function( result ) {
-                    expect( result ).to.be( _result );
+                    expect( result ).to.be.eql( _result );
                     expect( _calls ).to.be( 1 );
                 } );
         }, 100 );
 
         setTimeout( function() {
             var context = new de.Context.Base( {
-                cache: async_cache
+                cache: cache
             } );
             context.run( block )
                 .then( function( result ) {
-                    expect( result ).to.be( _result );
+                    expect( result ).to.be.eql( _result );
                     expect( _calls ).to.be( 2 );
 
                     done();
                 } );
-        }, 300 );
+        }, 1500 );
 
     } );
 
