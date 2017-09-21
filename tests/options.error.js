ec748cb53dd7a45fdfcc16d630d3fce321ebfe0b
Sergei Nikitin <nik.pasaran@gmail.com>
2017-09-21 16:14:11 +0300

    Возможность вернуть из options.error другую ошибку или не ошибку, а новое значение блока

--- tests/options.error.js
+++ tests/options.error.js
@@ -9,6 +9,8 @@ var helpers = require( './_helpers.js' );
 //  ---------------------------------------------------------------------------------------------------------------  //
 
 var ERROR_ID = 'ERROR_ERROR';
+var ANOTHER_ERROR_ID = 'ANOTHER_ERROR_ERROR';
+var YET_ANOTHER_ERROR_ID = 'YET_ANOTHER_ERROR_ERROR';
 
 //  ---------------------------------------------------------------------------------------------------------------  //
 
@@ -89,7 +91,164 @@ describe( 'options.error', function() {
 
                 done();
             } );
+    } );
 
+    it( 'multiple options.error, base and child return undefined', function( done ) {
+        var base_error_was_called = false;
+        var child_error_was_called = false;
+
+        const b1 = create_block(
+            helpers.wrap( function( params, context, state ) {
+                return de.error( ERROR_ID );
+            }, 50 ),
+            {
+                error: function( params, context, state, error ) {
+                    base_error_was_called = true;
+                }
+            }
+        );
+
+        const b2 = b1( {
+            options: {
+
+                error: function( params, context, state, result ) {
+                    child_error_was_called = true;
+                }
+
+            }
+        } );
+
+        var context = helpers.context();
+        context.run( b2 )
+            .then( function( result ) {
+                expect( result ).to.be.a( de.Error );
+                expect( result.error.id ).to.be.eql( ERROR_ID );
+                expect( base_error_was_called ).to.be.eql( true );
+                expect( child_error_was_called ).to.be.eql( true );
+
+                done();
+            } );
+    } );
+
+    it( 'multiple options.error, base returns error', function( done ) {
+        var base_error_was_called = false;
+        var child_error_was_called = false;
+
+        const b1 = create_block(
+            helpers.wrap( function( params, context, state ) {
+                return de.error( ERROR_ID );
+            }, 50 ),
+            {
+                error: function( params, context, state, error ) {
+                    base_error_was_called = true;
+
+                    return de.error( ANOTHER_ERROR_ID );
+                }
+            }
+        );
+
+        const b2 = b1( {
+            options: {
+
+                error: function( params, context, state, result ) {
+                    child_error_was_called = true;
+                }
+
+            }
+        } );
+
+        var context = helpers.context();
+        context.run( b2 )
+            .then( function( result ) {
+                expect( result ).to.be.a( de.Error );
+                expect( result.error.id ).to.be.eql( ANOTHER_ERROR_ID );
+                expect( base_error_was_called ).to.be.eql( true );
+                expect( child_error_was_called ).to.be.eql( true );
+
+                done();
+            } );
+    } );
+
+    it( 'multiple options.error, base and child return error', function( done ) {
+        var base_error_was_called = false;
+        var child_error_was_called = false;
+
+        const b1 = create_block(
+            helpers.wrap( function( params, context, state ) {
+                return de.error( ERROR_ID );
+            }, 50 ),
+            {
+                error: function( params, context, state, error ) {
+                    base_error_was_called = true;
+
+                    return de.error( ANOTHER_ERROR_ID );
+                }
+            }
+        );
+
+        const b2 = b1( {
+            options: {
+
+                error: function( params, context, state, result ) {
+                    child_error_was_called = true;
+
+                    return de.error( YET_ANOTHER_ERROR_ID );
+                }
+
+            }
+        } );
+
+        var context = helpers.context();
+        context.run( b2 )
+            .then( function( result ) {
+                expect( result ).to.be.a( de.Error );
+                expect( result.error.id ).to.be.eql( YET_ANOTHER_ERROR_ID );
+                expect( base_error_was_called ).to.be.eql( true );
+                expect( child_error_was_called ).to.be.eql( true );
+
+                done();
+            } );
+    } );
+
+    it( 'multiple options.error, base return value', function( done ) {
+        var base_error_was_called = false;
+        var child_error_was_called = false;
+
+        const b1 = create_block(
+            helpers.wrap( function( params, context, state ) {
+                return de.error( ERROR_ID );
+            }, 50 ),
+            {
+                error: function( params, context, state, error ) {
+                    base_error_was_called = true;
+
+                    return {
+                        foo: 42
+                    };
+                }
+            }
+        );
+
+        const b2 = b1( {
+            options: {
+
+                error: function( params, context, state, result ) {
+                    child_error_was_called = true;
+                }
+
+            }
+        } );
+
+        var context = helpers.context();
+        context.run( b2 )
+            .then( function( result ) {
+                expect( result ).to.not.be.a( de.Error );
+                expect( result ).to.be.eql( { foo: 42 } );
+                expect( base_error_was_called ).to.be.eql( true );
+                expect( child_error_was_called ).to.be.eql( false );
+
+                done();
+            } );
     } );
 
 } );
